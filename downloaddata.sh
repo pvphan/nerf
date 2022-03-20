a6b710bef34838ad703473768e6dc75aa2ef89ea
Vinh Phan <pvphan90@gmail.com>
2022-03-20 10:46:18 -0700

    removing file when done

--- downloaddata.sh
+++ downloaddata.sh
@@ -6,6 +6,6 @@ if [[ "$dataDirSize" -eq 0 ]]; then
     pushd ./data
     wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/tiny_nerf_data.npz
     wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/nerf_example_data.zip
-    unzip nerf_example_data.zip
+    unzip nerf_example_data.zip && rm nerf_example_data.zip
 fi
 
